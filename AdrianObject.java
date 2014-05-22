543396212b50199c6693dd6536c4908a67b781e7
Adrian3380 <umharr68@myumanitoba.ca>
2014-05-22 13:44:31 -0500

    Java Objects

---
+++ AdrianObject.java
@@ -0,0 +1,20 @@
+class AdrianObject
+{
+	public String initString = null;
+
+	public AdrianObject()
+	{
+		initString = "AdrianObject Active.";
+	}
+
+	public AdrianObject(String input)
+	{
+		initString = input;
+	}
+
+	public String toString()
+	{
+		return initString;
+	}
+
+}
