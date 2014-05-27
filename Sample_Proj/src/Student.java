2b3f97b8514b01a468ceca263d9274b7a5c1dc3e
unknown <v-dechin@dechin-lap2top.northamerica.corp.microsoft.com>
2014-05-27 16:12:24 -0500

    Another test commit

--- Sample_Proj/src/Student.java
+++ Sample_Proj/src/Student.java
@@ -1,4 +1,16 @@
 
 public class Student {
+	private String name;
+	private int studentNum;
+	
+	public Student (String name, int num)
+	{
+		this.name = name;
+		studentNum = num;
+	}
 
+	public String getName()
+	{
+		return name;
+	}
 }
